456eb41547c17e7cee003648227d62addeb8ce63
辻航大 <0xlaliberte@gmail.com>
2025-11-30 17:58:56 +0900

    Add `region`, `target_log_group_names`, and `target_log_group_arns` variables to S4 log archiver configuration

--- infrastructure/services/s4_log-archiver/variables.tf
+++ infrastructure/services/s4_log-archiver/variables.tf
@@ -1,10 +1,23 @@
 variable "name_prefix" { type = string }
 
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
 # 保存先 (DynamoDB)
 variable "log_archive_table_name" { type = string }
 variable "log_archive_table_arn" { type = string }
 
+# 収集対象のロググループ名
 variable "target_log_group_names" {
   description = "List of CloudWatch Log Group Names to query"
   type        = list(string)
+}
+
+# 親で target_log_group_arns = [] を渡しているので、定義が必要です
+variable "target_log_group_arns" {
+  description = "List of CloudWatch Log Group ARNs (Optional)"
+  type        = list(string)
+  default     = []
 }
