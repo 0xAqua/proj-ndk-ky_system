3cc7733f9c742540e77bfa9b292cc6c4d2995455
辻航大 <0xlaliberte@gmail.com>
2025-12-18 02:25:37 +0900

    Update handler name in S5 Admin User Lambda to `main.lambda_handler`

--- infrastructure/services/s5_admin-user/main.tf
+++ infrastructure/services/s5_admin-user/main.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "admin_user" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = var.name_prefix
   role             = aws_iam_role.lambda_role.arn
-  handler          = "main.handler"
+  handler          = "main.lambda_handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.12"
   timeout          = 30
